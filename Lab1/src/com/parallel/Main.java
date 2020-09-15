8223370aa252c3213073f9a0c01e355026205b27
Matas Savickis <savickismatas@gmail.com>
2020-09-15 23:30:31 +0300

    bj,nh

--- Lab1/src/com/parallel/Main.java
+++ Lab1/src/com/parallel/Main.java
@@ -3,64 +3,69 @@ package com.parallel;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
-	    System.out.println("Help me");
 	    VertybinisPopierius vp = new VertybinisPopierius(150.0f);
+        System.out.println("\nAkcininkai perkantys neapsaugant bendro resurso \n");
 	    try {
-            Thread akcininkasJonas = new Akcininkas(vp, 250.0f, "Jonas");
-            akcininkasJonas.start();
-
-            Thread akcininkasPetras = new Akcininkas(vp, 400.0f, "Petras");
-            akcininkasPetras.start();
-
-            akcininkasJonas.join();
-            akcininkasPetras.join();
+            Thread akcininkasPetras = new BlogasAkcininkas(vp,  "Petras"); akcininkasPetras.start();
+            Thread akcininkasJonas = new BlogasAkcininkas(vp,  "Jonas"); akcininkasJonas.start();
+            akcininkasJonas.join(); akcininkasPetras.join();
         } catch (Exception ex) {
 	        ex.printStackTrace();
+        }
+
+        System.out.println("\nAkcininkai perkantys apsaugant bendro resurso\n");
+        try {
+            Thread akcininkasPetras = new GerasAkcininkas(vp,  "Petras"); akcininkasPetras.start();
+            Thread akcininkasJonas = new GerasAkcininkas(vp,  "Jonas"); akcininkasJonas.start();
+            akcininkasJonas.join(); akcininkasPetras.join();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
 
-class Akcininkas extends Thread {
-
+class GerasAkcininkas extends Thread {
     private VertybinisPopierius vertybinisPopierius;
-    float santaupos;
     String vardas;
+    float turimosAkcijos = 0;
 
-    public Akcininkas(VertybinisPopierius vertybinisPopierius, float santaupos, String vardas) {
+    public GerasAkcininkas(VertybinisPopierius vertybinisPopierius, String vardas) {
         this.vertybinisPopierius = vertybinisPopierius;
-        this.santaupos = santaupos;
         this.vardas = vardas;
     }
 
-//    public void run() {
-//        for(int i = 0 ; i < 10; i++) {
-//            float akcijosKaina = vertybinisPopierius.kaina;
-//            try {
-//                Thread.sleep(new Random().nextInt(500));
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//           // if (vertybinisPopierius.kaina < santaupos) {
-//                System.out.println(vardas + " nusipirko " + vertybinisPopierius.pirkti(akcijosKaina) + "akciju");
-//           // }
-//        }
-//    }
-
-        public void run() {
-        for(int i = 0 ; i < 10; i++) {
+    public void run() {
+        for(int i = 0 ; i < 100; i++) {
             synchronized(vertybinisPopierius) {
-                float akcijosKaina = vertybinisPopierius.kaina;
-                try {
-                    Thread.sleep(new Random().nextInt(500));
-                } catch (InterruptedException e) {
-                }
-                System.out.println(vardas + " nusipirko " + vertybinisPopierius.pirkti(akcijosKaina) + "akciju");
+                float akcijosKaina = vertybinisPopierius.kaina; //KRITINE SEKCIJA
+                try { Thread.sleep(new Random().nextInt(50)); } catch (InterruptedException e) { }
+                turimosAkcijos += vertybinisPopierius.pirkti(akcijosKaina);
             }
         }
+        System.out.println(vardas + " nusipirko " + turimosAkcijos + " akciju");
     }
 
+}
+
+class BlogasAkcininkas extends Thread {
+    private VertybinisPopierius vertybinisPopierius;
+    String vardas;
+    float turimosAkcijos = 0;
+
+    public BlogasAkcininkas(VertybinisPopierius vertybinisPopierius, String vardas) {
+        this.vertybinisPopierius = vertybinisPopierius;
+        this.vardas = vardas;
+    }
+
+    public void run() {
+        for(int i = 0 ; i < 100; i++) {
+            float akcijosKaina = vertybinisPopierius.kaina; //KRITINE SEKCIJA
+            try { Thread.sleep(new Random().nextInt(50)); } catch (InterruptedException e) { }
+            turimosAkcijos += vertybinisPopierius.pirkti(akcijosKaina);
+        }
+        System.out.println(vardas + " nusipirko " + turimosAkcijos + " akciju");
+    }
 }
 
 class VertybinisPopierius {
